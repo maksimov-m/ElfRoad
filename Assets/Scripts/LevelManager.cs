6571762ae30df75d1f41ac8f93894b1a6c4ffa3c
Khaziev Ravil <100825440+RavilKhaziev@users.noreply.github.com>
2023-12-07 19:02:38 +0500

    Start project

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    public Transform mainMenu, optionMenu, characterMenu, rankingMenu;
+    public Transform viewMenu, musicMenu;
+    public Transform musicOn, musicOff;
+    public InputField userInputField;
+
+    public AudioSource audio;
+
+    public string username;
+
+    Scene PlayScene;
+
+    public void Start() 
+    {
+        var music = MusicControl.Instance();
+        if (music.AudioSource == null)
+        {
+            DontDestroyOnLoad(audio);
+            audio.loop = true;
+            music.AudioSource = audio;
+        }
+        music.PlayBackgroundSound();
+        
+    }
+
+    public void NameClarify() {
+        //userInputField.text = "Enter PlayerName Here...";
+        username = userInputField.text.ToString();
+        PlayerPrefs.SetString("Name", username);
+        //print(username);
+        print(PlayerPrefs.GetString("Name"));
+    }
+
+    public void LoadScene(string name) {
+        
+        SceneManager.LoadScene(name);
+
+    }
+
+    public void QuitGame()  {
+        Application.Quit();
+    }
+
+    public void OptionMenu(bool clicked) {
+        if (clicked == true) {
+            optionMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(false);
+        }
+        else {
+            optionMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(true);
+        }
+    }
+
+    public void character(bool clicked) {
+        if (clicked == true) {
+            optionMenu.gameObject.SetActive(clicked);
+            characterMenu.gameObject.SetActive(false);
+        }
+        else {
+            optionMenu.gameObject.SetActive(clicked);
+            characterMenu.gameObject.SetActive(true);
+        }
+    }
+
+    public void rankMenu(bool clicked) {
+        if (clicked == true) {
+            rankingMenu.gameObject.SetActive(clicked);
+            optionMenu.gameObject.SetActive(false);
+        }
+        else {
+            rankingMenu.gameObject.SetActive(clicked);
+            optionMenu.gameObject.SetActive(true);
+        }
+    }
+
+    public void ViewMenu(bool clicked) {
+        if (clicked == true) {
+            viewMenu.gameObject.SetActive(clicked);
+            optionMenu.gameObject.SetActive(false);
+        }
+        else {
+            viewMenu.gameObject.SetActive(clicked);
+            optionMenu.gameObject.SetActive(true);
+        }
+    }
+
+    public void MusicMenu(bool clicked) {
+        if (clicked == true) {
+            musicMenu.gameObject.SetActive(clicked);
+            optionMenu.gameObject.SetActive(false);
+        }
+        else {
+            musicMenu.gameObject.SetActive(clicked);
+            optionMenu.gameObject.SetActive(true);
+        }
+    }
+
+    public void MusicOn(bool clicked) {
+        //if (clicked == true) {
+            audio.Play();
+        //}
+    }
+
+    public void MusicOff(bool clicked) {
+        //if (clicked == true) {
+            audio.Pause();
+        //}
+    }
+}
---
+++ Assets/Scripts/MusicControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditorInternal;
+using System;
+
+
+public class MusicControl {
+
+    private static MusicControl _instance;
+    private AudioSource _audioSource;
+    private DateTime _time;  
+    public AudioSource AudioSource 
+    {
+        get { return _audioSource; }
+        set { _audioSource = value; }
+    }
+    public bool IsPlayMusic { get; set; } = false;
+    private MusicControl() { }
+
+    public static MusicControl Instance() 
+    {
+        if (_instance == null)
+            _instance = new MusicControl();
+        return _instance; 
+    }
+
+    public void PlayBackgroundSound()
+    {
+
+        if (_audioSource != null)
+        {
+            IsPlayMusic = true;
+            _audioSource?.Play();
+        }
+    }
+
+    public void StopBackgroundSound() 
+    {
+       IsPlayMusic = false;
+       AudioSource?.Stop();
+    }
+
+}
